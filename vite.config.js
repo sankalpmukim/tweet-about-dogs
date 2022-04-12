06659f40421f2df3b5677bd0b20ebf2f8a70a047
Sankalp Mukim <sankalpmukim@gmail.com>
2022-04-12 20:56:28 +0530

    Fix deploy base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vtc-website-2022/",
+  base: "/tweet-about-dogs/",
   plugins: [react()],
 });
